5523e76b874d3449fd9d35d9d9ee098536d1b5ec
Mateus Resende <mateus.rsnd@gmail.com>
2016-05-10 09:10:53 -0300

    Arruma migracao de banho

--- db/migrate/20160426092647_add_user_id_to_bath.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToBath < ActiveRecord::Migration
-  def change
-    add_column :baths, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426092647) do
+ActiveRecord::Schema.define(version: 20160510120732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20160426092647) do
     t.date     "date"
     t.float    "ideal_amount"
     t.datetime "time_spent"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
     t.integer  "user_id"
   end
 
